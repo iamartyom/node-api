10c6eb9f1b91a7fc1b98bbae753dc249fd1303d6
temagr <postagric@gmail.com>
2017-12-19 14:08:13 +0300

    Update note routes

--- routes/note_routes.js
+++ routes/note_routes.js
@@ -1,19 +1,44 @@
+const mongo = require('mongodb');
+
 module.exports = function(app, db) {
     app.get('/notes/:id', (req, res) => {
-        res.send("Meow");
+        const filterData = {'_id': new mongo.ObjectID(req.params.id)};
+
+        db.collection('notes').findOne(filterData, (err, result) => {
+            if (err) { res.send(err); }
+                
+            res.send(result);
+        });
     });
 
     app.post('/notes', (req, res) => {
         const note = { title: req.body.title, text: req.body.body };
 
         db.collection("notes").insert(note, (err, result) => {
-            if (err)
-            {
-                res.send(err);
-            }
-            else {
-                res.send(result);
-            }
+            if (err) { res.send(err); }
+            
+            res.send(result);
+        });
+    });
+
+    app.put('/notes/:id', (req, res) => {
+        const filterData = {'_id': new mongo.ObjectID(req.params.id)};
+        const note = { title: req.body.title, text: req.body.body };
+
+        db.collection("notes").update(filterData, note, (err, result) => {
+            if (err) { res.send(err); }
+
+            res.send(result);
+        });
+    })
+
+    app.delete('/notes/:id', (req, res) => {
+        const filterData = {'_id': new mongo.ObjectID(req.params.id)};
+
+        db.collection('notes').remove(filterData, (err, result) => {
+            if (err) { res.send(err); }
+
+            res.send(result);
         });
     });
 };
